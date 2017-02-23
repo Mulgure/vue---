abc044526eb246ce669878fa675e69624c89a904
Mulgure <Mulgure@users.noreply.github.com>
2017-02-23 12:49:55 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+new Vue({
+  el: '#demo-2',
+  data: {
+    parentMsg: 'Message from parent'
+  },
+  components: {
+    child: {
+      props: ['myMessage'],
+      template: '<span>{{myMessage}}</span>'
+    }
+  }
+})
